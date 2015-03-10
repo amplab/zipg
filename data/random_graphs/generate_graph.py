d6823b9b07142378eba6510660b40e434c6a8644
Evan Ye <applepie31415@gmail.com>
2015-03-10 00:19:41 -0700

    fast generation for regular graphs over 1mil nodes

--- data/random_graphs/generate_graph.py
+++ data/random_graphs/generate_graph.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import numpy as np
 from networkx import random_regular_graph
 
 if len(sys.argv) < 3:
@@ -10,10 +11,31 @@ if len(sys.argv) < 3:
 num_nodes = int(sys.argv[1])
 node_degree = int(sys.argv[2])
 
-graph = random_regular_graph(node_degree, num_nodes)
 edge_list = []
-for edge in graph.edges():
-    edge_list.append('{0} {1}\n'.format(edge[0], edge[1]))
+if num_nodes <= 1000000:
+    graph = random_regular_graph(node_degree, num_nodes)
+    for edge in graph.edges():
+        edge_list.append('{0} {1}\n'.format(edge[0], edge[1]))
+else:
+    n = num_nodes
+    node_degree_left = np.ones(num_nodes) * node_degree
+    nodes_remaining = np.array(range(n))
+    while n > 0:
+        from_node_idx = np.random.randint(0, n)
+        from_node = nodes_remaining[from_node_idx]
+        degree = node_degree_left[from_node]
+        nodes_remaining[from_node_idx] = nodes_remaining[n-1]
+        n -= 1
+        to_node_idxs = np.random.randint(0, n, degree)
+        for to_node_idx in to_node_idxs:
+            to_node = nodes_remaining[to_node_idx]
+            node_degree_left[to_node] -= 1
+            edge_list.append('{0} {1}\n'.format(from_node, to_node))
+        for to_node_idx in to_node_idxs:
+            to_node = nodes_remaining[to_node_idx]
+            if node_degree_left[to_node] == 0:
+                nodes_remaining[to_node_idx] = nodes_remaining[n-1]
+                n -= 1
 
 with open(str(num_nodes) + '.txt', 'w') as f:
     f.writelines(edge_list)
