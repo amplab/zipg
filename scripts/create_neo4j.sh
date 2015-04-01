0469334706bdcbbf1331af2eaebbaab7ba041e4d
Evan Ye <applepie31415@gmail.com>
2015-04-01 03:05:21 +0000

    fixed ec2 scripts

--- scripts/create_neo4j.sh
+++ scripts/create_neo4j.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-for nodes in 1000 10000 100000 1000000
+for nodes in 10000 100000 1000000 2000000
 do
-	sed 's/\([0-9]*\) \([0-9]*\)/\1,\2,E/' ${nodes}.edge > ${nodes}_edge.csv 
+	sed 's/\([0-9]*\) \([0-9]*\)/\1,\2,E/' ${nodes}.edge > csv/${nodes}_edge.csv 
 	freq=1
-	while [ $freq -le $nodes ]
+	while [ $freq -lt $nodes ]
 	do
-		../bin/create_nodes ${nodes} ${nodes} ${freq}
-		awk '{printf("%d,%s\n", NR-1, $0)}' ${nodes}_${freq}.node > ${nodes}_${freq}_node.csv
-		../../neo4j/bin/neo4j-import --into ~/target/${nodes}_${freq} --nodes:Nodes nodes-header.csv,${nodes}_${freq}_node.csv --relationships edges-header.csv,${nodes}_edge.csv --id-type INTEGER
-		../../neo4j/bin/neo4j-shell -path ~/target/${nodes}_${freq} -c "CREATE INDEX ON :Node(name);" 
+		awk '{printf("%d,%s\n", NR-1, $0)}' ${nodes}_${freq}.node > csv/${nodes}_${freq}_node.csv
+		./neo4j/bin/neo4j-import --into ~/target/${nodes}_${freq} --nodes:Node csv/nodes-header.csv,csv/${nodes}_${freq}_node.csv --relationships csv/edges-header.csv,csv/${nodes}_edge.csv --id-type INTEGER
+		./neo4j/bin/neo4j-shell -path ~/target/${nodes}_${freq} -c "CREATE INDEX ON :Node(name);" 
 		freq=$((freq * 10))
 	done
 done
--- scripts/create_succinct.sh
+++ scripts/create_succinct.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-for nodes in 1000 
+for nodes in 2000000 3000000 4000000 #10000 100000 1000000 2000000 3000000 4000000 5000000
 do
 	freq=1
 	while [ $freq -lt $nodes ]
 	do
 		echo creating succinct graph: ${nodes}_${freq}
-                ../bin/create nodes ${nodes} ${freq}
-		../bin/create succinct ${nodes}_${freq}.node ${nodes}.edge
+                ./succinct-graph/bin/create nodes ${nodes} ${freq}
+		./succinct-graph/bin/create succinct ${nodes}_${freq}.node ${nodes}.edge
 		freq=$((freq * 10))
 	done
 done
