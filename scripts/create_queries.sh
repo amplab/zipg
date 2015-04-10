d2754a85e26b528bd45d9ffe9f43c731956d003d
Evan Ye <applepie31415@gmail.com>
2015-04-10 01:21:53 -0700

    typo in creating queries

--- scripts/create_queries.sh
+++ scripts/create_queries.sh
@@ -4,5 +4,5 @@ for num_nodes in ${nodes[@]}
 do
     echo creating queries for graph: ${num_nodes}
     ${bin}/create node-queries ${data}/nodes/${num_nodes}.node ${data}/node-queries/warmup_${num_nodes}.txt ${data}/node/queries/query_${num_nodes}.txt
-    ${bin}/create neighbor-queries ${data}/nodes/${num_nodes}.node ${data}/neighbor-queries/warmup_${num_nodes}.txt ${data}/node/neighbor-queries/query_${num_nodes}.txt
+    ${bin}/create neighbor-queries ${num_nodes} ${data}/neighbor-queries/warmup_${num_nodes}.txt ${data}/node/neighbor-queries/query_${num_nodes}.txt
 done
