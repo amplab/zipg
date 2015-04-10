00644ee491cf9eebf2eadd5f577f2ef83970733a
Evan Ye <applepie31415@gmail.com>
2015-04-10 01:33:13 -0700

    one more create query typo

--- scripts/create_queries.sh
+++ scripts/create_queries.sh
@@ -3,6 +3,6 @@ source config.sh
 for num_nodes in ${nodes[@]} 
 do
     echo creating queries for graph: ${num_nodes}
-    ${bin}/create node-queries ${data}/nodes/${num_nodes}.node ${data}/node-queries/warmup_${num_nodes}.txt ${data}/node/queries/query_${num_nodes}.txt
-    ${bin}/create neighbor-queries ${num_nodes} ${data}/neighbor-queries/warmup_${num_nodes}.txt ${data}/node/neighbor-queries/query_${num_nodes}.txt
+    ${bin}/create node-queries ${data}/nodes/${num_nodes}.node ${data}/node-queries/warmup_${num_nodes}.txt ${data}/node-queries/query_${num_nodes}.txt
+    ${bin}/create neighbor-queries ${num_nodes} ${data}/neighbor-queries/warmup_${num_nodes}.txt ${data}/neighbor-queries/query_${num_nodes}.txt
 done
