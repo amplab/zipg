5097e7c8148409fc615d3236303fe06eca9bb396
Evan Ye <applepie31415@gmail.com>
2015-05-11 21:14:46 -0700

    changed neo4j target directory to neo4j

--- scripts/create_neo4j.sh
+++ scripts/create_neo4j.sh
@@ -5,9 +5,9 @@ do
     echo "Creating csv for ${num_nodes} nodes"
     sed 's/\([0-9]*\) \([0-9]*\)/\1,\2,E/' ${data}/edges/${num_nodes}.edge > ${data}/csv/${num_nodes}_edge.csv 
     awk '{printf("%d,%s\n", NR-1, $0)}' ${data}/nodes/${num_nodes}.node > ${data}/csv/${num_nodes}_node.csv
-    ${dir}/external/neo4j/bin/neo4j-import --into ${data}/target/${num_nodes} --nodes:Node ${data}/csv/nodes-header.csv,${data}/csv/${num_nodes}_node.csv --relationships ${data}/csv/edges-header.csv,${data}/csv/${num_nodes}_edge.csv --id-type INTEGER
+    ${dir}/external/neo4j/bin/neo4j-import --into ${data}/neo4j/${num_nodes} --nodes:Node ${data}/csv/nodes-header.csv,${data}/csv/${num_nodes}_node.csv --relationships ${data}/csv/edges-header.csv,${data}/csv/${num_nodes}_edge.csv --id-type INTEGER
     for (( i = 0; i < ${attributes}; i++ ))
     do
-        ${dir}/external/neo4j/bin/neo4j-shell -path ${data}/target/${num_nodes} -c "CREATE INDEX ON :Node(name${i});" 
+        ${dir}/external/neo4j/bin/neo4j-shell -path ${data}/neo4j/${num_nodes} -c "CREATE INDEX ON :Node(name${i});" 
     done
 done
