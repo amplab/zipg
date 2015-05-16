98fe0b9bca804794f3cbfb943e56d0021c37d663
EC2 Default User <ec2-user@ip-10-186-154-84.ec2.internal>
2015-05-16 00:49:47 +0000

    update succinct benching scripts

--- scripts/bench_succinct.sh
+++ scripts/bench_succinct.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 source config.sh
-for num_nodes in ${nodes[@]}
+num_nodes=10000000
+for graph in ${nodes[@]}
 do
-    echo "Benching ${num_nodes}, freq ${freq}"
-    #${bin}/bench -t name-throughput -w ${data}/queries/warmup_${num_nodes}_${freq}.txt -q ${data}/queries/query_${num_nodes}_${freq}.txt -o ${dir}/name_throughput_results.txt ${data}/succinct/${num_nodes}_${freq}.graph
-    ${bin}/bench -t mix-latency -x ${warmup_mix} -y ${measure_mix} -z ${cooldown_mix} -w ${data}/node-queries/warmup_${num_nodes}.txt -q ${data}/node-queries/query_${num_nodes}.txt -a ${data}/neighbor-queries/warmup_${num_nodes}.txt -b ${data}/neighbor-queries/query_${num_nodes}.txt -o ${dir}/${num_nodes}_mix_throughput_results.txt ${data}/succinct/${num_nodes}.graph
+    echo "Benching ${num_nodes}"
+    ${bin}/bench -t neighbor-latency -x ${warmup_neighbor} -y ${measure_neighbor} -z ${cooldown_neighbor} -w ${data}/neighbor-queries/warmup_${num_nodes}.txt -q ${data}/neighbor-queries/query_${num_nodes}.txt -o ${dir}/${graph}_neighbor_latency_results.txt ${data}/succinct/${graph}.graph
+    ${bin}/bench -t node-latency -x ${warmup_node} -y ${measure_node} -z ${cooldown_node} -w ${data}/node-queries/warmup_${num_nodes}.txt -q ${data}/node-queries/query_${num_nodes}.txt -o ${dir}/${graph}_name_latency_results.txt ${data}/succinct/${graph}.graph
+    ${bin}/bench -t mix-latency -x ${warmup_mix} -y ${measure_mix} -z ${cooldown_mix} -w ${data}/node-queries/warmup_${num_nodes}.txt -q ${data}/node-queries/query_${num_nodes}.txt -a ${data}/neighbor-queries/warmup_${num_nodes}.txt -b ${data}/neighbor-queries/query_${num_nodes}.txt -o ${dir}/${graph}_mix_latency_results.txt ${data}/succinct/${graph}.graph
 done
