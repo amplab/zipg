09a950b97b11e576b9fccc906dec7003aec9f549
Zongheng Yang <zongheng.y@gmail.com>
2015-06-04 12:59:26 -0700

    create_neo4j.sh: reuse csv if possible; add --stacktrace to neo4j-import.

--- scripts/create_neo4j.sh
+++ scripts/create_neo4j.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+set -e
+
 SCRIPT_DIR=$(dirname $0)
 source ${SCRIPT_DIR}/config.sh
+
 mkdir -p ${NEO4J_DIR}
+
 for num_nodes in ${nodes[@]}
 do
+    if [ -d ${NEO4J_DIR}/${num_nodes} ]
+    then
+        echo "Warning: neo4j database ${NEO4J_DIR}/${num_nodes} exists, import will \
+not force write; if you really want a new database, remove it first then \
+re-run this script"
+        exit
+    fi
+
     echo "Creating neo4j-import csv for ${num_nodes} nodes"
     node_header=":ID"
     for (( i = 0; i < ${attributes}; i++ ))
@@ -12,12 +24,29 @@ do
     done
     echo $node_header > ${CSV_DIR}/nodes-header.csv
 
-    sed 's/\([0-9]*\) \([0-9]*\)/\1,\2,E/' ${EDGE_DIR}/${num_nodes}.edge > ${CSV_DIR}/${num_nodes}_edge.csv
-    awk '{printf("%d,%s\n", NR-1, $0)}' ${NODE_DIR}/${num_nodes}.node > ${CSV_DIR}/${num_nodes}_node.csv
+    EDGE_CSV=${CSV_DIR}/${num_nodes}_edge.csv
+    if [ -s $EDGE_CSV ]
+    then
+        echo $EDGE_CSV already exists, using it instead of generating from .edge
+    else
+        # TODO: this sed is very slow, can just patch the OCaml program
+        sed 's/\([0-9]*\) \([0-9]*\)/\1,\2,E/' ${EDGE_DIR}/${num_nodes}.edge > $EDGE_CSV
+    fi
+
+    NODE_CSV=${CSV_DIR}/${num_nodes}_node.csv
+    if [ -s $NODE_CSV ]
+    then
+        echo $NODE_CSV already exists, using it instead of generating from .node
+    else
+        # input (line i): [attr]
+        # output (line i): i-1, [attr]
+        awk '{printf("%d,%s\n", NR-1, $0)}' ${NODE_DIR}/${num_nodes}.node > $NODE_CSV
+    fi
+
     ${HOME_DIR}/external/neo4j/bin/neo4j-import --into ${NEO4J_DIR}/${num_nodes} \
-        --nodes:Node ${CSV_DIR}/nodes-header.csv,${CSV_DIR}/${num_nodes}_node.csv \
-        --relationships ${CSV_DIR}/edges-header.csv,${CSV_DIR}/${num_nodes}_edge.csv \
-        --id-type INTEGER
+        --nodes:Node "${CSV_DIR}/nodes-header.csv,${CSV_DIR}/${num_nodes}_node.csv" \
+        --relationships "${CSV_DIR}/edges-header.csv,${CSV_DIR}/${num_nodes}_edge.csv" \
+        --id-type INTEGER --stacktrace
 
     # create indexes on all attributes
     for (( i = 0; i < ${attributes}; i++ ))
