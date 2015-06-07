9c63e85fe2670c41b2310afa8884d3307840d698
Zongheng Yang <zongheng.y@gmail.com>
2015-06-07 14:57:45 -0700

    Clean up `deg` definition in scripts: use mathematical def from now on.

--- scripts/config.sh
+++ scripts/config.sh
@@ -15,31 +15,35 @@ QUERY_DIR=${DATA_DIR}/queries
 
 # graph construction configs
 nodes=( 100000 ) # list of num_nodes to bench
-deg=20
+deg=40 # average outgoing degree (so, total 2 * (# of directed edges) = deg * num_nodes)
 freq=100
 attributes=10
 attribute_length=32
 
 # succinct construction configs
-sa_sampling_rate=32
-isa_sampling_rate=32
-npa_sampling_rate=128
+sa_sampling_rate=64
+isa_sampling_rate=64
+npa_sampling_rate=256
 
 # benchmark configs
 JVM_HEAP=16g
+
 warmup_neighbor=100000
 measure_neighbor=200000
 neo4j_warmup_neighbor=500000
 neo4j_measure_neighbor=500000
+
 warmup_node=5000
 measure_node=10000
 neo4j_warmup_node=100000
 neo4j_measure_node=100000
-warmup_mix=5000
-measure_mix=10000
-neo4j_warmup_mix=100000
-neo4j_measure_mix=200000
+
 warmup_neighbor_node=100000
 measure_neighbor_node=200000
 neo4j_warmup_neighbor_node=100000
 neo4j_measure_neighbor_node=200000
+
+warmup_mix=5000
+measure_mix=10000
+neo4j_warmup_mix=100000
+neo4j_measure_mix=200000
--- scripts/create_edges.sh
+++ scripts/create_edges.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
+set -e
+
 SCRIPT_DIR=$(dirname $0)
 source ${SCRIPT_DIR}/config.sh
 mkdir -p ${EDGE_DIR}
 
-# NOTE: `deg` is not really degree: it's really [actual avg outgoing degree / 2]. 
-# Just view this as saying we need deg*num_nodes undirected edges (and the 
-# `generate_graphs` program will output twice that many, directed.
 for num_nodes in ${nodes[@]}
 do
     echo "Creating ${EDGE_DIR}/${num_nodes}.edge"
-    ${BIN_DIR}/generate_graphs ${num_nodes} $((deg * num_nodes))
+    ${BIN_DIR}/generate_graphs ${num_nodes} $((deg * num_nodes / 2))
     mv ${num_nodes}.edge ${EDGE_DIR}
 done
