6453442b82dce42099bb22a19f49104973ae4ea6
Zongheng Yang <zongheng.y@gmail.com>
2015-06-07 15:15:50 -0700

    Drop cache in bench_succinct.sh as well.

--- scripts/bench_succinct.sh
+++ scripts/bench_succinct.sh
@@ -8,11 +8,13 @@ for num_nodes in ${nodes[@]}
 do
     echo "Benching ${num_nodes}"
 
+    sync && sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
     ${BIN_DIR}/bench -t neighbor-latency -x ${warmup_neighbor} \
 		-y ${measure_neighbor} -w ${QUERY_DIR}/neighbor_warmup_${num_nodes}.txt \
 		-q ${QUERY_DIR}/neighbor_query_${num_nodes}.txt \
 		-o ${HOME_DIR}/${num_nodes}_neighbor_latency.txt ${SUCCINCT_DIR}/${num_nodes}.graph
 
+    sync && sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
     ${BIN_DIR}/bench -t node-latency -x ${warmup_node} \
 		-y ${measure_node} -w ${QUERY_DIR}/node_warmup_${num_nodes}.txt \
 		-q ${QUERY_DIR}/node_query_${num_nodes}.txt \
@@ -25,11 +27,13 @@ do
         # -b ${QUERY_DIR}/neighbor_query_${num_nodes}.txt \
 		# -o ${dir}/${num_nodes}_mix_latency.txt ${SUCCINCT_DIR}/${num_nodes}.graph
 
+    sync && sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
     ${BIN_DIR}/bench -t node-node-latency -x ${warmup_node} \
 		-y ${measure_node} -w ${QUERY_DIR}/node_warmup_${num_nodes}.txt \
 		-q ${QUERY_DIR}/node_query_${num_nodes}.txt \
 		-o ${HOME_DIR}/${num_nodes}_double_node_latency.txt ${SUCCINCT_DIR}/${num_nodes}.graph
 
+    sync && sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
     ${BIN_DIR}/bench -t neighbor-node-latency -x ${warmup_neighbor_node} \
 		-y ${measure_neighbor_node} -w ${QUERY_DIR}/neighbor_node_warmup_${num_nodes}.txt \
 		-q ${QUERY_DIR}/neighbor_node_query_${num_nodes}.txt \
