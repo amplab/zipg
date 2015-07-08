15947807a94b3e1543be571589c46e037839aa5e
Zongheng Yang <zongheng.y@gmail.com>
2015-07-08 15:24:56 -0700

    First cut at GraphQueryAggregator: w/o cluster-wide routing

--- src/rpc/GraphQueryAggregator.cpp
+++ src/rpc/GraphQueryAggregator.cpp
@@ -1,10 +1,14 @@
 #include "thrift/GraphQueryAggregatorService.h"
-#include <thrift/protocol/TBinaryProtocol.h>
-#include <thrift/server/TSimpleServer.h>
-#include <thrift/transport/TServerSocket.h>
-#include <thrift/transport/TBufferTransports.h>
 
+#include <fstream>
+
+#include <thrift/protocol/TBinaryProtocol.h>
+#include <thrift/server/TThreadedServer.h>
+#include <thrift/transport/TBufferTransports.h>
+#include <thrift/transport/TServerSocket.h>
 #include <thrift/transport/TSocket.h>
+
+#include "rpc/ports.h"
 #include <thrift/GraphQueryService.h>
 
 using namespace ::apache::thrift;
@@ -18,86 +22,183 @@ class GraphQueryAggregatorServiceHandler :
     virtual public GraphQueryAggregatorServiceIf {
 
 public:
-    GraphQueryAggregatorServiceHandler() {
-        // Your initialization goes here
+    GraphQueryAggregatorServiceHandler(
+        int total_num_shards,
+        int local_num_shards,
+        int local_host_id,
+        const std::vector<std::string>& hostnames)
+    : total_num_shards_(total_num_shards),
+      local_num_shards_(local_num_shards),
+      local_host_id_(local_host_id),
+      hostnames_(hostnames),
+      total_num_hosts_(hostnames.size())
+    { }
+
+    int32_t connect_to_local_shards() {
+        for (int i = 0; i < local_num_shards_; i++) {
+            fprintf(stderr, "Connecting to local server %d...", i);
+            try {
+                shared_ptr<TSocket> socket(new TSocket(
+                    "localhost", QUERY_SERVER_PORT + i));
+                shared_ptr<TTransport> transport(
+                    new TBufferedTransport(socket));
+                shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+                GraphQueryServiceClient client(protocol);
+
+                transport->open();
+                fprintf(stderr, "Connected!\n");
+                local_shards_.push_back(client);
+                fprintf(stderr, "Pushed!\n");
+            } catch (std::exception& e) {
+                fprintf(stderr, "Could not connect to server: %s\n", e.what());
+                return 1;
+            }
+        }
+        fprintf(
+            stderr,
+            "Currently have %zu local server connections.\n",
+            local_shards_.size());
+        return 0;
     }
 
-    int32_t connect_to_local_servers() {
-        // Your implementation goes here
-        printf("connect_to_local_servers\n");
+    void get_neighbors(std::vector<int64_t> & _return, const int64_t nodeId) {
+        int shard_id = nodeId % total_num_shards_;
+        int host_id = shard_id % total_num_hosts_;
+        if (host_id == local_host_id_) {
+            local_shards_.at(shard_id / total_num_hosts_)
+                .get_neighbors(_return, nodeId);
+        } else {
+            assert(false && "routing not implemented");
+        }
     }
 
-    void get_neighbors_local(
-        std::vector<int64_t> & _return,
-        const int64_t nodeId)
-    {
-        // Your implementation goes here
-        printf("get_neighbors_local\n");
-    }
-
-    void get_neighbors_atype_local(
+    void get_neighbors_atype(
         std::vector<int64_t> & _return,
         const int64_t nodeId,
         const int64_t atype)
     {
-        // Your implementation goes here
-        printf("get_neighbors_atype_local\n");
+        int shard_id = nodeId % total_num_shards_;
+        int host_id = shard_id % total_num_hosts_;
+        if (host_id == local_host_id_) {
+            local_shards_.at(shard_id / total_num_hosts_)
+                .get_neighbors_atype(_return, nodeId, atype);
+        } else {
+            assert(false && "routing not implemented");
+        }
     }
 
-    void get_neighbors_attr_local(
+    void get_neighbors_attr(
         std::vector<int64_t> & _return,
         const int64_t nodeId,
         const int32_t attrId,
         const std::string& attrKey)
     {
-        // Your implementation goes here
-        printf("get_neighbors_attr_local\n");
+        assert(false && "not implemented"); // TODO
     }
 
-    void get_nodes_local(
+    void get_nodes(
         std::set<int64_t> & _return,
         const int32_t attrId,
         const std::string& attrKey)
     {
-        // Your implementation goes here
-        printf("get_nodes_local\n");
+        assert(false && "not implemented"); // TODO
     }
 
-    void get_nodes2_local(
+    void get_nodes2(
         std::set<int64_t> & _return,
         const int32_t attrId1,
         const std::string& attrKey1,
         const int32_t attrId2,
         const std::string& attrKey2)
     {
-        // Your implementation goes here
-        printf("get_nodes2_local\n");
+        assert(false && "not implemented"); // TODO
     }
 
+private:
+    const int total_num_shards_; // total # of logical shards
+    const int local_num_shards_;
+
+    // id of this physical node
+    const int local_host_id_;
+    // all aggregators in the cluster, hostnames used for opening sockets
+    std::vector<std::string> hostnames_;
+    const int total_num_hosts_;
+
+    std::vector<GraphQueryServiceClient> local_shards_;
 };
 
-int main(int argc, char **argv) {
-
-    shared_ptr<TSocket> socket(new TSocket("localhost", 9090));
-    shared_ptr<TTransport> transport(new TBufferedTransport(socket));
-    shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
-    GraphQueryServiceClient qsclient(protocol);
-    transport->open();
-    fprintf(stderr, "Connected!\n");
-    std::vector<int64_t> nhbrs;
-    qsclient.get_neighbors(nhbrs, 1);
-    fprintf(stderr, "Pushed!\n");
-
-// From prototype:
-//  int port = 9090;
-//  shared_ptr<GraphQueryAggregatorServiceHandler> handler(new GraphQueryAggregatorServiceHandler());
-//  shared_ptr<TProcessor> processor(new GraphQueryAggregatorServiceProcessor(handler));
-//  shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
-//  shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
-//  shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
-//
-//  TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
-//  server.serve();
-  return 0;
+void print_usage(char *exec) {
+    fprintf(
+        stderr,
+        "Usage: %s [-t total_num_shards] [-s local_num_shards] "
+        "[-h hostsfile] [-i local_host_id]\n",
+        exec);
 }
 
+int main(int argc, char **argv) {
+    if (argc < 2 || argc > 10) {
+        print_usage(argv[0]);
+        return -1;
+    }
+
+    int c, total_num_shards, local_num_shards, local_host_id;
+    std::string hostsfile;
+    while ((c = getopt(argc, argv, "t:s:i:h:")) != -1) {
+        switch(c) {
+        case 't':
+            total_num_shards = atoi(optarg);
+            break;
+        case 's':
+            local_num_shards = atoi(optarg);
+            break;
+        case 'i':
+            local_host_id = atoi(optarg);
+            break;
+        case 'h':
+            hostsfile = optarg;
+            break;
+        default:
+            total_num_shards = local_num_shards = 1;
+            local_host_id = 0;
+            hostsfile = "./conf/hosts";
+        }
+    }
+
+    std::ifstream hosts(hostsfile);
+    std::string host;
+    std::vector<std::string> hostnames;
+    while (std::getline(hosts, host, '\n')) {
+        hostnames.push_back(host);
+    }
+
+    int port = QUERY_HANDLER_PORT;
+    try {
+        shared_ptr<GraphQueryAggregatorServiceHandler> handler(
+            new GraphQueryAggregatorServiceHandler(
+                total_num_shards,
+                local_num_shards,
+                local_host_id,
+                hostnames));
+
+        shared_ptr<TProcessor> processor(
+            new GraphQueryAggregatorServiceProcessor(handler));
+        shared_ptr<TServerSocket> server_transport(new TServerSocket(port));
+        shared_ptr<TBufferedTransportFactory> transport_factory(
+            new TBufferedTransportFactory());
+        shared_ptr<TProtocolFactory> protocol_factory(
+            new TBinaryProtocolFactory());
+        TThreadedServer server(
+            processor,
+            server_transport,
+            transport_factory,
+            protocol_factory);
+
+        server.serve();
+    } catch (std::exception& e) {
+        fprintf(
+            stderr,
+            "Exception at GraphQueryAggregator:main(): %s\n",
+            e.what());
+    }
+    return 0;
+}
