2163bad64299317a72aa1a133f65d185a4e9e3b6
Zongheng Yang <zongheng.y@gmail.com>
2015-07-08 21:18:29 -0700

    Guarding absence of binary in sbin/partition-input.sh

--- sbin/partition-input.sh
+++ sbin/partition-input.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+set -e
 
 sbin="`dirname "$0"`"
 sbin="`cd "$sbin"; pwd`"
 source "$sbin/succinct-config.sh"
 source "$SUCCINCT_PREFIX/sbin/load-succinct-env.sh"
 
-$SUCCINCT_HOME/bin/graph-partitioner \
+prog=$SUCCINCT_HOME/bin/graph-partitioner
+if [ ! -f "$prog" ]; then
+  >&2 echo "Binary graph-partitioner hasn't been built yet: run \`make graph-partitioner\`?"
+  exit 1
+fi
+
+$prog \
   -n $TOTAL_NUM_SHARDS \
   $NODE_FILE \
   $EDGE_FILE
