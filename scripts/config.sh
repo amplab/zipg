eb13c81ce888fd3649dce9f015f514ec17e140f5
Zongheng Yang <zongheng.y@gmail.com>
2015-07-24 03:29:48 +0000

    Check in config.sh changes

--- scripts/config.sh
+++ scripts/config.sh
@@ -15,17 +15,52 @@ QUERY_DIR=${DATA_DIR}/queries
 
 NODE_FILE=${DATA_DIR}/higgs-2attr350each-tpch.node
 EDGE_FILE=${DATA_DIR}/higgs-social_network.edge_table
-IS_NODE_FILE_CSV=0
+
+
+# npa 64, sa/isa 16
+NODE_FILE=${DATA_DIR}/higgs-2attr350each-tpch-npa64saIsa16.node
+EDGE_FILE=${DATA_DIR}/higgs-social_network.lessPad.noPadHeaderWidths-npa64saIsa16.edge_table
+
+# npa 128, sa/isa 32
+NODE_FILE=${DATA_DIR}/higgs-2attr350each-tpch-npa128saIsa32.node
+EDGE_FILE=${DATA_DIR}/higgs-social_network.lessPad.noPadHeader-npa128saIsa32.edge_table # also, no pad 2 widths
+
+# npa 16, sa/isa4
+NODE_FILE=${DATA_DIR}/higgs-2attr350each-tpch-npa16saIsa4.node
+EDGE_FILE=${DATA_DIR}/higgs-social_network.lessPad.noPadHeaderWidths-npa16saIsa4.edge_table
+
+# npa 32, sa/isa8
+NODE_FILE=${DATA_DIR}/higgs-2attr350each-tpch-npa32saIsa8.node
+EDGE_FILE=${DATA_DIR}/higgs-social_network.lessPad.noPadHeaderWidths-npa32saIsa8.edge_table
+
+# level 0
+NODE_FILE=${DATA_DIR}/higgs-2attr350each-tpch-npa128sa32isa64.nodeWithPtrs
+EDGE_FILE=${DATA_DIR}/higgs-social_network.opts-npa128sa32isa64.edge_table
+
+# live journal
+NODE_FILE=${DATA_DIR}/liveJournal-40attr16each-tpch.node
+
+# 20attr, 35 each
+NODE_FILE=${DATA_DIR}/higgs-20attr35each-tpch.node
+EDGE_FILE=${DATA_DIR}/higgs-social_network.opts-npa32sa2isa32.edge_table
+
+# 40attr, 16 each, higgs
+NODE_FILE=${DATA_DIR}/higgs-40attr16each-tpch.node
+
+ASSOC_FILE=${DATA_DIR}/liveJournal.assoc # used for formatting neo4j
 ASSOC_FILE=${DATA_DIR}/higgs-social_network.assoc # used for formatting neo4j
+
 # \x02
 NEO4J_DELIM=
+IS_NODE_FILE_CSV=0
 
 # graph construction configs
 nodes=( 100000 ) # list of num_nodes to bench
 deg=40 # average outgoing degree (so, total 2 * (# of directed edges) = deg * num_nodes)
+
 freq=100
-attributes=2
-attribute_length=350
+attributes=40
+attribute_length=16
 max_num_atype=5
 
 # succinct construction configs
@@ -38,26 +73,28 @@ JVM_HEAP=16g
 
 warmup_neighbor=0
 warmup_neighbor=100000
-measure_neighbor=2000
+measure_neighbor=200
 measure_neighbor=200000
-neo4j_warmup_neighbor=20000 # 5k is not sufficient
-neo4j_measure_neighbor=100000
+neo4j_warmup_neighbor=100000 # 5k is not sufficient
+neo4j_measure_neighbor=200000
 
-warmup_node=1000
+warmup_node=0
 warmup_node=50000
 measure_node=2000
 measure_node=100000
-neo4j_warmup_node=20000 # 5k is not sufficient
+neo4j_warmup_node=50000 # 5k is not sufficient
 neo4j_measure_node=100000
 
-warmup_neighbor_node=10
+warmup_neighbor_node=100
 warmup_neighbor_node=100000
 measure_neighbor_node=2000
 measure_neighbor_node=200000
 neo4j_warmup_neighbor_node=100000
 neo4j_measure_neighbor_node=200000
 
+warmup_neighbor_atype=0
 warmup_neighbor_atype=100000
+measure_neighbor_atype=2000
 measure_neighbor_atype=200000
 neo4j_warmup_neighbor_atype=100000
 neo4j_measure_neighbor_atype=200000
