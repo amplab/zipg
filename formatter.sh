9d22f5b4c3ea1c1a439df2017660c9cba4fe41e6
Zongheng Yang <zongheng.y@gmail.com>
2015-09-09 07:01:35 +0000

    Fixes/changes for twitter-2010 in formatter.sh
    Including a loop that check result sizes

--- formatter.sh
+++ formatter.sh
@@ -8,12 +8,14 @@ dataset=twitter2010-40attr16each
 
 if [[ "$dataset" == "twitter2010-40attr16each" ]]; then
   edgelist=TODO
+  num_nodes=1000000
   num_nodes=41652230
+  num_node_attr=1
   num_node_attr=40
-  node_attr_freq=1000 # FIXME
+  zipf_corpus_size=1000
   node_attr_size_each=16
-  inner_delim='	'
-  end_delim=''
+  inner_delim='	' # tab
+  end_delim='^M'
   assoc_out_file=TODO
   node_out_file=/mnt2/twitter2010-${num_node_attr}attr${node_attr_size_each}each-tpch.node
 elif [[ "$dataset" == "liveJournal-40attr16each" ]]; then
@@ -41,33 +43,49 @@ else
 fi
 
 i=0
-#for f in /vol0/twitter/part-*; do
-for num in `seq 0 1 194`; do
+for num in `seq 0 1 0`; do
   padded=$(printf "%0*d" 5 ${num})
-  f="/vol0/twitter/part-${padded}"
+  f="part-${padded}"
   assoc_out_file=${f}.assoc
-  if [[ -f "$assoc_out_file" ]]; then
-    echo $assoc_out_file exists, skipping
-    continue
-  fi
+#  if [[ -f "/mnt2T/twitter/$assoc_out_file" ]]; then
+#    echo /mnt2T/twitter/$assoc_out_file exists, skipping
+#    continue
+#  fi
   echo i=$i, working on file "${f}"
 
-  ./bin/create \
+  ./benchmark/bin/create \
     format-input \
-    ${f} \
-    /vol0/data_0 \
-    /mnt2/${assoc_out_file} \
+    /mnt2/twitter/${f} \
+    data/data_0 \
+    /mnt2T/twitter/${assoc_out_file} \
     data/${node_out_file} \
     $num_nodes \
     $num_node_attr \
-    $node_attr_freq \
+    $zipf_corpus_size \
     $node_attr_size_each \
     "${inner_delim}" \
     "${end_delim}" &
   i=$((i + 1))
-  if [[ ( "$i" == 15 ) ]]; then
+  if [[ ( "$i" == 30 ) ]]; then
     echo "waiting for batch"
     wait
     i=0
   fi
 done
+wait
+
+# check sizes
+#for num in `seq 0 1 194`; do
+#  padded=$(printf "%0*d" 5 ${num})
+#  f="part-${padded}"
+#  assoc_out_file=${f}.assoc
+#
+#  assocLines=$(wc -l /mnt2T/twitter/${assoc_out_file} | cut -d' ' -f1)
+#  inputLines=$(wc -l /mnt2/twitter/${f} | cut -d' ' -f1)
+#  
+#  if [[ ! ( $assocLines == $inputLines ) ]]; then
+#    echo for file $f, lines not equal: $assocLines, $inputLines
+#  fi
+#
+#done
+#echo "Sizes check done!"
