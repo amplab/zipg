738a05412350d8acf8871dcee4c91445ad969306
Zongheng Yang <zongheng.y@gmail.com>
2015-09-13 19:08:40 +0000

    Change config.sh: double # queries for warmup & measure

--- scripts/config.sh
+++ scripts/config.sh
@@ -3,6 +3,7 @@ SCRIPT_DIR=$(dirname $0)
 HOME_DIR=${SCRIPT_DIR}/..
 
 BIN_DIR=${HOME_DIR}/bin
+BENCH_BIN_DIR=${HOME_DIR}/benchmark/bin
 DATA_DIR=${HOME_DIR}/data
 
 NODE_DIR=${DATA_DIR}/nodes
@@ -37,9 +38,6 @@ EDGE_FILE=${DATA_DIR}/higgs-social_network.lessPad.noPadHeaderWidths-npa32saIsa8
 NODE_FILE=${DATA_DIR}/higgs-2attr350each-tpch-npa128sa32isa64.nodeWithPtrs
 EDGE_FILE=${DATA_DIR}/higgs-social_network.opts-npa128sa32isa64.edge_table
 
-# live journal
-NODE_FILE=${DATA_DIR}/liveJournal-40attr16each-tpch.node
-
 # 20attr, 35 each
 NODE_FILE=${DATA_DIR}/higgs-20attr35each-tpch.node
 EDGE_FILE=${DATA_DIR}/higgs-social_network.opts-npa32sa2isa32.edge_table
@@ -47,9 +45,19 @@ EDGE_FILE=${DATA_DIR}/higgs-social_network.opts-npa32sa2isa32.edge_table
 # 40attr, 16 each, higgs
 NODE_FILE=${DATA_DIR}/higgs-40attr16each-tpch.node
 
-# source assoc file, used for neo4j creation & query gen.
-ASSOC_FILE=${DATA_DIR}/higgs-social_network.assoc
-ASSOC_FILE=${DATA_DIR}/liveJournal.assoc
+# live journal
+NODE_FILE=/mnt2T/data/liveJournal-40attr16each-tpch.node
+
+# source assoc file, used for neo4j creation 
+ASSOC_FILE=/mnt2T/data/liveJournal-minDeg30.assoc
+ASSOC_FILE=/mnt2T/data/liveJournal-minDeg45.assoc
+ASSOC_FILE=/mnt2T/data/liveJournal-minDeg60.assoc
+ASSOC_FILE=/mnt2T/data/liveJournal.assoc 
+ASSOC_FILE=/mnt2T/data/liveJournal-minDeg30WithTsAttr.assoc
+
+# twitter-2010, for neo4j / query creation
+NODE_FILE=/mnt2T/twitter2010-40attr16each-tpch.node
+ASSOC_FILE=/mnt2T/twitter/twitter2010-npa128sa32isa64.assoc
 
 # \x02
 NEO4J_DELIM=
@@ -73,53 +81,57 @@ npa_sampling_rate=256
 JVM_HEAP=16g
 
 warmup_neighbor=0
-warmup_neighbor=100000
+warmup_neighbor=200000
 measure_neighbor=200
-measure_neighbor=200000
+measure_neighbor=400000
 neo4j_warmup_neighbor=100000 # 5k is not sufficient
 neo4j_measure_neighbor=200000
 
 warmup_node=0
-warmup_node=50000
+warmup_node=200000
 measure_node=2000
-measure_node=100000
+measure_node=400000
 neo4j_warmup_node=50000 # 5k is not sufficient
 neo4j_measure_node=100000
 
 warmup_neighbor_node=100
-warmup_neighbor_node=100000
+warmup_neighbor_node=200000
 measure_neighbor_node=2000
-measure_neighbor_node=200000
+measure_neighbor_node=400000
 neo4j_warmup_neighbor_node=100000
 neo4j_measure_neighbor_node=200000
 
 warmup_neighbor_atype=0
-warmup_neighbor_atype=100000
+warmup_neighbor_atype=200000
 measure_neighbor_atype=2000
-measure_neighbor_atype=200000
+measure_neighbor_atype=400000
 neo4j_warmup_neighbor_atype=100000
 neo4j_measure_neighbor_atype=200000
 
-warmup_mix=2000
-measure_mix=400000
+warmup_mix=400000
+measure_mix=800000
 neo4j_warmup_mix=100000
 neo4j_measure_mix=400000
 
 ## TAO queries
-warmup_assocRange=100000
-measure_assocRange=200000
+warmup_assocRange=0
+warmup_assocRange=200000
+measure_assocRange=5
+measure_assocRange=400000
 
-warmup_objGet=100000
-measure_objGet=200000
+warmup_objGet=200000
+measure_objGet=400000
 
-warmup_assocGet=100000
-measure_assocGet=200000
+warmup_assocGet=200000
+measure_assocGet=400000
 
-warmup_assocCount=100000
-measure_assocCount=200000
+warmup_assocCount=0
+warmup_assocCount=200000
+measure_assocCount=5
+measure_assocCount=400000
 
-warmup_assocTimeRange=100000
-measure_assocTimeRange=200000
+warmup_assocTimeRange=200000
+measure_assocTimeRange=400000
 
-warmup_taoMix=200000
-measure_taoMix=400000
+warmup_taoMix=400000
+measure_taoMix=800000
