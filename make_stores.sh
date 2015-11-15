a902d609f4b2af3a65a28082e577ad3db8747e45
Zongheng Yang <zongheng.y@gmail.com>
2015-11-15 01:23:38 -0800

    Script changes

--- make_stores.sh
+++ make_stores.sh
@@ -4,6 +4,11 @@ make -j bench
 
 dataset=twitter2010-40attr16each
 
+num_suff_stores=1
+num_edges_per_suff_store=1
+num_log_stores=1
+num_edges_per_log_store=1
+
 if [[ "$dataset" == "twitter2010-40attr16each" ]]; then
   store_out="${dataset}"
   num_nodes=41652230
@@ -17,32 +22,44 @@ else
   exit 1
 fi
 
-# Suffix Store
-store_out_file="${store_out}.suffixstore"
-num_edges_to_add=1
-./benchmark/bin/create make-store \
-  ${store_out_file} \
-  ${num_edges_to_add} \
-  ${num_nodes} \
-  ${num_atypes} \
-  ${assoc_set_file} \
-  ${attr_file} \
-  ${bytes_per_attr} \
-  ${min_time} \
-  ${max_time} \
-  0 # for suffix store
+# SuffixStores
 
-# Log Store
-store_out_file="${store_out}.logstore"
-num_edges_to_add=1
-./benchmark/bin/create make-store \
-  ${store_out_file} \
-  ${num_edges_to_add} \
-  ${num_nodes} \
-  ${num_atypes} \
-  ${assoc_set_file} \
-  ${attr_file} \
-  ${bytes_per_attr} \
-  ${min_time} \
-  ${max_time} \
-  1 # for log store
+padWidth=${#num_suff_stores}
+
+for i in $(seq 1 1 ${num_suff_stores}); do
+  p=$(printf "%0*d" ${padWidth} ${i})
+  store_out_file="${store_out}.suffixstore-part${p}of${num_suff_stores}"
+
+  ./benchmark/bin/create make-store \
+    ${store_out_file} \
+    ${num_edges_per_suff_store} \
+    ${num_nodes} \
+    ${num_atypes} \
+    ${assoc_set_file} \
+    ${attr_file} \
+    ${bytes_per_attr} \
+    ${min_time} \
+    ${max_time} \
+    0 # for suffix store
+done
+
+# LogStores
+
+padWidth=${#num_suff_stores}
+
+for i in $(seq 1 1 ${num_log_stores}); do
+  p=$(printf "%0*d" ${padWidth} ${i})
+  store_out_file="${store_out}.logstore-part${p}of${num_log_stores}"
+
+  ./benchmark/bin/create make-store \
+    ${store_out_file} \
+    ${num_edges_per_log_store} \
+    ${num_nodes} \
+    ${num_atypes} \
+    ${assoc_set_file} \
+    ${attr_file} \
+    ${bytes_per_attr} \
+    ${min_time} \
+    ${max_time} \
+    1 # for log store
+done
