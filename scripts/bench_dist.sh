3a814623113fbe6a0b3f663742bb3230490d1ac1
Anurag Khandelwal <anuragk@berkeley.edu>
2016-04-01 00:50:38 +0000

    Separated benchmark script from copy script

--- scripts/bench_dist.sh
+++ scripts/bench_dist.sh
@@ -11,7 +11,6 @@ set -ex
 # 5. Set the desired settings in rates-bench.sh
 
 npa=128; sa=32; isa=64 # L0, by default
-copyShardFiles=T
 
 #node_file_raw=/mnt2/uk-2007-05-40attr16each-tpch-npa128sa32isa64.node
 #edge_file_raw=/mnt2/uk-2007-05-40attr16each-npa128sa32isa64.assoc
@@ -32,95 +31,9 @@ benches=(
   #benchEdgeAttrsThput
 )
 
-# NOTE: settings here only affects this master killing all
-# servers when the time's up.  The real times should be set
-# in the code (e.g. GraphBenchmark), and recompile accordingly.
-# in secs
-# thputWarm=60
-# thputMeasure=180
-# thputCool=30
-
-#### Initial setup
-
 currDir=$(cd $(dirname $0); pwd)
 . "${currDir}/../sbin/succinct-config.sh"
 . "${currDir}/../sbin/load-succinct-env.sh"
-
-#### Copy the corresponding shard files over
-if [[ -n $copyShardFiles ]]; then
-  echo "Copying shard files..."
-  num_succinctstore_hosts=$num_hosts
-
-  limit=$(($TOTAL_NUM_SHARDS - 1))
-  padWidth=${#TOTAL_NUM_SHARDS}
-
-  echo "Shard id range 0-$limit"
-  echo "Number of Succinct Store Hosts: $num_succinctstore_hosts"
-  
-  echo "Beginning to copy in 10s..."
-  sleep 10
-
-  for shard_id in `seq 0 $limit`; do
-      # transfer shard id i to an appropriate host
-      host_id=$(($shard_id % num_succinctstore_hosts))
-      host=$(sed -n "$(($host_id + 1)){p;q;}" ${currDir}/../conf/hosts)
-  
-      padded_shard_id=$(printf "%0*d" ${padWidth} ${shard_id})
-      node_split="${node_file_raw}-part${padded_shard_id}of${TOTAL_NUM_SHARDS}"
-      edge_split="${edge_file_raw}-part${padded_shard_id}of${TOTAL_NUM_SHARDS}"
-  
-      # Encoded succinct dirs
-      # Hacky: note this uses internal impl details about namings of encoded tables
-      nodeTbl="${node_split}WithPtrs.succinct"
-      # This replaces the last 'assoc' by 'edge_table'
-      edgeTbl=$(echo -n "${edge_split}.succinct" | sed 's/\(.*\)assoc\(.*\)/\1edge_table\2/')
-
-      if [[ ! -d "${nodeTbl}" ]]; then
-        echo "dir ${nodeTbl} doesn't exist, exiting"
-        exit 1
-      fi
-      if [[ ! -d "${edgeTbl}" ]]; then
-        echo "dir ${edgeTbl} doesn't exist, exiting"
-        exit 1
-      fi
-  
-      echo "shard_id: ${shard_id}, host_id ${host_id}"
-
-      d1=$(dirname "${nodeTbl}")
-      d2=$(dirname "${edgeTbl}")
-      rsync -arL ${nodeTbl} ${host}:$d1 &
-      rsync -arL ${edgeTbl} ${host}:$d2 &
-  done
-
-  if [[ "$ENABLE_MULTI_STORE" == T ]]; then
-    # copy logstore data
-    limit=$((NUM_LOGSTORE_PARTS - 1))
-    # Copy to last succinct store server
-    host=$(sed -n "$((num_succinctstore_hosts)){p;q;}" ${currDir}/../conf/hosts)
-    for i in $(seq 0 "${limit}"); do
-      padWidth=${#NUM_LOGSTORE_PARTS}
-      padded_shard_id=$(printf "%0*d" ${padWidth} ${i})
-      nodeTbl="${node_file_raw}.logstore-part${padded_shard_id}of${NUM_LOGSTORE_PARTS}_logstore"
-      edgeTbl="${edge_file_raw}.logstore-part${padded_shard_id}of${NUM_LOGSTORE_PARTS}_logstore"
-      d1=$(dirname "${nodeTbl}")
-      d2=$(dirname "${edgeTbl}")
-      if [ -e "$nodeTbl" ]; then
-        rsync -arL ${nodeTbl} ${host}:$d1 &
-      else
-        echo "WARNING: LogStore node table not found"
-      fi
-
-      if [ -e "$edgeTbl" ]; then
-        rsync -arL ${edgeTbl} ${host}:$d2 &
-      else
-        echo "WARNING: LogStore edge table not found"
-      fi
-    done
-  fi
-
-  wait
-  echo "Shard files copied to all servers."
-fi
 
 #### Launch aggregator & shards on all hosts
 
