1e57939d7a9f8658e7e8710e7b3f260afb559375
Anurag Khandelwal <anuragk@berkeley.edu>
2016-04-09 21:10:35 -0700

    Updated setup script

--- scripts/setup_dist.sh
+++ scripts/setup_dist.sh
@@ -9,8 +9,6 @@ node_file_raw=/mnt2/twitter2010-40attr16each-tpch.node
 edge_file_raw=/mnt2/twitter2010-npa128sa32isa64.assoc
 
 currDir=$(cd $(dirname $0); pwd)
-. "${currDir}/../sbin/succinct-config.sh"
-. "${currDir}/../sbin/load-succinct-env.sh"
 
 function stop_all() {
   bash ${currDir}/../sbin/stop-all.sh 
@@ -18,27 +16,9 @@ function stop_all() {
 }
 
 function start_all() {
-  ${currDir}/../sbin/start-servers.sh $node_file_raw $edge_file_raw $sa $isa $npa
-  sleep 2
-
-  ${currDir}/../sbin/start-handlers.sh 
-  sleep 2
-
-  ${currDir}/../sbin/load-data.sh
-  sleep 2
-
-  # note: the script launch order is important
-  ${currDir}/../sbin/backfill-updates.sh
+  bash ${currDir}/../sbin/start-handlers.sh $node_file_raw $edge_file_raw $sa $isa $npa 
   sleep 2
 }
-
-function timestamp() {
-  date +"%D-%T"
-}
-
-bash ${currDir}/../sbin/hosts.sh source "${currDir}/../sbin/succinct-config.sh"
-bash ${currDir}/../sbin/hosts.sh source "${currDir}/../sbin/load-succinct-env.sh"
-sleep 2
 
 stop_all
 start_all
