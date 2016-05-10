5bba35e42218c476a4c10fef4f984df2994309c9
Anurag Khandelwal <anuragk@berkeley.edu>
2016-05-10 09:38:54 -0700

    Fixed AssocAdd latency benchmark

--- neo4j/benchmark/src/main/java/edu/berkeley/cs/succinctgraph/neo4jbench/tao/BenchTAOAssocAdd.java
+++ neo4j/benchmark/src/main/java/edu/berkeley/cs/succinctgraph/neo4jbench/tao/BenchTAOAssocAdd.java
@@ -21,12 +21,14 @@ public class BenchTAOAssocAdd {
   static Random warmupRand;
   static Random rand;
   public static List<String> EDGE_ATTR;
+
   static {
     char[] chars = new char[128];
     Arrays.fill(chars, '|');
     EDGE_ATTR = new ArrayList<>();
     EDGE_ATTR.add(new String(chars));
   }
+
   static int numWarmupQueries, numMeasureQueries;
 
   public static void main(String[] args) {
@@ -34,64 +36,60 @@ public class BenchTAOAssocAdd {
     String dbPath = args[1];
     String outputFile = args[2];
     NUM_NODES = Long.parseLong(args[3]);
-    String neo4jPageCacheMemory = args[4];
-
-    numWarmupQueries = 10000;
-    numMeasureQueries = 1000;
+    boolean tuned = Boolean.valueOf(args[4]);
+    String neo4jPageCacheMemory = args[5];
+    numWarmupQueries = 100000;
+    numMeasureQueries = 10000;
     warmupRand = new Random(SEED);
     rand = new Random(SEED + 1);
 
     if (type.equals("latency")) {
-      benchAssocCountLatency(dbPath, neo4jPageCacheMemory, outputFile);
+      benchAssocCountLatency(tuned, dbPath, neo4jPageCacheMemory, outputFile);
     } else {
       System.err.println("Unknown type: " + type);
     }
   }
 
-  private static void benchAssocCountLatency(
-    String dbPath, String neo4jPageCacheMem, String outputFile) {
+  private static void benchAssocCountLatency(boolean tuned, String dbPath, String neo4jPageCacheMem,
+    String outputFile) {
 
     System.out.println("Benchmarking assoc_add() queries");
-    System.out.println("Setting Neo4j's dbms.pagecache.memory: " +
-      neo4jPageCacheMem);
 
-    GraphDatabaseService db = new GraphDatabaseFactory()
-      .newEmbeddedDatabaseBuilder(dbPath)
-      .setConfig(GraphDatabaseSettings.cache_type, "none")
-      .setConfig(
-        GraphDatabaseSettings.pagecache_memory, neo4jPageCacheMem)
-      .newGraphDatabase();
+    GraphDatabaseService db;
+    System.out.println("About to open database");
+    if (tuned) {
+      db = new GraphDatabaseFactory().newEmbeddedDatabaseBuilder(dbPath)
+        .setConfig(GraphDatabaseSettings.cache_type, "none")
+        .setConfig(GraphDatabaseSettings.pagecache_memory, neo4jPageCacheMem).newGraphDatabase();
+    } else {
+      db = new GraphDatabaseFactory().newEmbeddedDatabase(dbPath);
+    }
+    System.out.println("Done opening");
 
     BenchUtils.registerShutdownHook(db);
     Transaction tx = db.beginTx();
     try {
       BenchUtils.fullWarmup(db);
 
-      PrintWriter out = new PrintWriter(new BufferedWriter(
-        new FileWriter(outputFile)));
+      PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(outputFile)));
 
       PrintWriter resOut = null;
       if (System.getenv("BENCH_PRINT_RESULTS") != null) {
-        resOut = new PrintWriter(new BufferedWriter(
-          new FileWriter(outputFile + ".neo4j_result")));
+        resOut = new PrintWriter(new BufferedWriter(new FileWriter(outputFile + ".neo4j_result")));
       }
 
-      System.out.println(
-        "Warming up for " + numWarmupQueries + " queries");
+      System.out.println("Warming up for " + numWarmupQueries + " queries");
       for (int i = 0; i < numWarmupQueries; ++i) {
-        if (i % 10000 == 0) {
-          tx.success();
-          tx.close();
-          tx = db.beginTx();
-        }
+        tx.success();
+        tx.close();
+        tx = db.beginTx();
         long id1 = Math.abs(warmupRand.nextLong()) % NUM_NODES;
         long id2 = Math.abs(warmupRand.nextLong()) % NUM_NODES;
         int atype = warmupRand.nextInt(5);
         taoImpls.assocAdd(db, id1, id2, atype, System.currentTimeMillis(), EDGE_ATTR);
       }
 
-      System.out.println(
-        "Measuring for " + numMeasureQueries + " queries");
+      System.out.println("Measuring for " + numMeasureQueries + " queries");
       for (int i = 0; i < numMeasureQueries; ++i) {
         if (i % 10000 == 0) {
           tx.success();
@@ -108,10 +106,7 @@ public class BenchTAOAssocAdd {
         out.println("1," + microsecs);
 
         if (resOut != null) {
-          resOut.printf("%d %d %d\n",
-            id1,
-            id2,
-            atype);
+          resOut.printf("%d %d %d\n", id1, id2, atype);
         }
       }
       out.close();
