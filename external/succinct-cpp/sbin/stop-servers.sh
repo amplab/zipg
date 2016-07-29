9c40c5d35953633d745b155b8b78f401a1f614e2
Anurag Khandelwal <anuragk@berkeley.edu>
2016-07-29 16:51:22 -0700

    Removed unnecessary succinct files

--- external/succinct-cpp/sbin/stop-servers.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-pids="`pgrep ${QUERY_SERVER_PROCESS}`"
-
-for pid in $pids
-do
-	echo "Killing pid $pid..."
-	kill -9 $pid
-done
--- external/succinct-cpp/sharded/include/ports.h
+++
@@ -1,6 +0,0 @@
-#ifndef PORTS_H
-#define PORTS_H
-
-#define AGGREGATOR_PORT      11001
-#define SERVER_PORT       11002
-#endif
