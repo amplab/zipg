aa678d701ad237f5e0ba0de336de65bbb8e18eb4
Anurag Khandelwal <anuragk@berkeley.edu>
2016-07-31 16:04:39 -0700

    Added auto-complete to zipg-shell

--- client/py/preamble.py
+++ client/py/preamble.py
@@ -49,4 +49,27 @@ except Thrift.TException, tx:
   print '%s' % (tx.message)
   print 'Check your server status and retry connecting with zipgConnect(host, port)'
 
+# Add auto-completion and a stored history file of commands to your Python
+# interactive interpreter. Requires Python 2.0+, readline. Autocomplete is
+# bound to the Esc key by default (you can change it - see readline docs).
+#
+# Store the file in ~/.pystartup, and set an environment variable to point
+# to it:  "export PYTHONSTARTUP=~/.pystartup" in bash.
 
+import atexit
+import readline
+import rlcompleter
+
+historyPath = os.path.expanduser("~/.pyhistory")
+
+def save_history(historyPath=historyPath):
+    import readline
+    readline.write_history_file(historyPath)
+
+if os.path.exists(historyPath):
+    readline.read_history_file(historyPath)
+
+atexit.register(save_history)
+readline.parse_and_bind('tab: complete')
+
+del os, atexit, readline, rlcompleter, save_history, historyPath, sys
