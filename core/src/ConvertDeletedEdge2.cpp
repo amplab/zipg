0d063f0aedb154bbc41efdf12a29007fdf1e6e7c
Anurag Khandelwal <anuragk@berkeley.edu>
2016-08-07 19:22:05 -0700

    Added alternate converter

---
+++ core/src/ConvertDeletedEdge2.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <fstream>
+#include <cstdio>
+#include <cstdint>
+#include <cstdlib>
+
+#include "bitmap.h"
+#include "DeletedEdges.h"
+
+int main(int argc, char** argv) {
+  std::string filename = argv[1];
+
+  std::ifstream in(filename);
+  std::ofstream out(filename + ".new");
+  std::vector<int64_t> offsets;
+  std::vector<DeletedEdges::edge_record_id> edge_record_ids;
+  int64_t cur_offset = 0;
+  while (!in.eof()) {
+    int64_t src, atype;
+    in.read(reinterpret_cast<char *>(&src), sizeof(int64_t));
+    in.read(reinterpret_cast<char *>(&atype), sizeof(int64_t));
+    bitmap::Bitmap *b = new bitmap::Bitmap();
+    size_t num_edges = b->Deserialize(in);
+
+    DeletedEdges::edge_record_id rec = { src, atype };
+    edge_record_ids.push_back(rec);
+    offsets.push_back(cur_offset);
+    cur_offset += num_edges;
+    delete b;
+  }
+
+  std::cout << "Total number of edges = " << cur_offset << "\n";
+
+  DeletedEdges del(edge_record_ids, offsets, cur_offset);
+
+  std::cout << "Created deleted edges with " << del.GetNumEdges()
+            << " edges and " << del.GetNumRecords() << " edges.\n";
+  del.Serialize(out);
+
+  out.close();
+
+  return 0;
+}
