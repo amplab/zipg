bc589d005b24f018ad4056c05294a90a92993b58
Anurag Khandelwal <anuragk@berkeley.edu>
2016-08-09 16:52:58 -0700

    Better SEGFAULT logging

--- rpc/src/GraphQueryAggregator.cpp
+++ rpc/src/GraphQueryAggregator.cpp
@@ -1,9 +1,11 @@
 #include "GraphQueryAggregatorService.h"
 
-#include <stdio.h>
 #include <execinfo.h>
 #include <signal.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ucontext.h>
 #include <unistd.h>
 
 #include <fstream>
@@ -1655,6 +1657,52 @@ std::string edge_part_name(std::string base_path, int32_t shard_id,
   return edge_part.str();
 }
 
+typedef struct _sig_ucontext {
+  unsigned long uc_flags;
+  struct ucontext *uc_link;
+  stack_t uc_stack;
+  struct sigcontext uc_mcontext;
+  sigset_t uc_sigmask;
+} sig_ucontext_t;
+
+void crit_err_hdlr(int sig_num, siginfo_t * info, void * ucontext) {
+  void * array[50];
+  void * caller_address;
+  char ** messages;
+  int size, i;
+  sig_ucontext_t * uc;
+
+  uc = (sig_ucontext_t *) ucontext;
+
+  /* Get the address at the time the signal was raised */
+#if defined(__i386__) // gcc specific
+  caller_address = (void *) uc->uc_mcontext.eip;  // EIP: x86 specific
+#elif defined(__x86_64__) // gcc specific
+  caller_address = (void *) uc->uc_mcontext.rip;  // RIP: x86_64 specific
+#else
+#error Unsupported architecture. // TODO: Add support for other arch.
+#endif
+
+  fprintf(stderr, "signal %d (%s), address is %p from %p\n", sig_num,
+          strsignal(sig_num), info->si_addr, (void *) caller_address);
+
+  size = backtrace(array, 50);
+
+  /* overwrite sigaction with caller's address */
+  array[1] = caller_address;
+
+  messages = backtrace_symbols(array, size);
+
+  /* skip first stack frame (points here) */
+  for (i = 1; i < size && messages != NULL; ++i) {
+    fprintf(stderr, "[bt]: (%d) %s\n", i, messages[i]);
+  }
+
+  free(messages);
+
+  exit(EXIT_FAILURE);
+}
+
 void handler(int sig) {
   void *array[10];
   size_t size = backtrace(array, 10);
@@ -1670,7 +1718,17 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  signal(SIGSEGV, handler);
+  struct sigaction sigact;
+
+  sigact.sa_sigaction = crit_err_hdlr;
+  sigact.sa_flags = SA_RESTART | SA_SIGINFO;
+
+  if (sigaction(SIGSEGV, &sigact, (struct sigaction *) NULL) != 0) {
+    LOG_E("error setting signal handler for %d (%s)\n", SIGSEGV,
+          strsignal(SIGSEGV));
+
+    exit(EXIT_FAILURE);
+  }
 
   LOG_E("Launched aggregator with command line: ");
   for (int i = 0; i < argc; i++) {
