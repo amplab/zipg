54c4ca3dec3359afc0cbf0cb7027a44187c0023b
Anurag Khandelwal <anuragk@berkeley.edu>
2017-02-16 01:06:15 +0000

    Parallel build

--- ec2/install-dependencies.sh
+++ ec2/install-dependencies.sh
@@ -8,6 +8,7 @@ yum -y install libevent libevent-devel python-devel zlib-devel openssl-devel\
   mysql-devel ruby-devel rubygems java-devel cmake wget gcc-c++
 
 # Create temporary directory for install files
+rm -rf tmpdir
 mkdir tmpdir
 cd tmpdir
 
@@ -16,7 +17,7 @@ wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
 tar xvf autoconf-2.69.tar.gz
 cd autoconf-2.69
 ./configure --prefix=/usr
-make
+make -j
 make install
 cd ..
 
@@ -25,7 +26,7 @@ wget http://ftp.gnu.org/gnu/automake/automake-1.14.tar.gz
 tar xvf automake-1.14.tar.gz
 cd automake-1.14
 ./configure --prefix=/usr
-make
+make -j
 make install
 cd ..
 
@@ -34,7 +35,7 @@ wget http://ftp.gnu.org/gnu/bison/bison-2.5.1.tar.gz
 tar xvf bison-2.5.1.tar.gz
 cd bison-2.5.1
 ./configure --prefix=/usr
-make
+make -j
 make install
 cd ..
 
@@ -43,7 +44,7 @@ wget http://sourceforge.net/projects/boost/files/boost/1.55.0/boost_1_55_0.tar.g
 tar xvf boost_1_55_0.tar.gz
 cd boost_1_55_0
 ./bootstrap.sh
-./b2 install
+./b2 -j install
 cd ..
 
 # Manually install latest thrift
@@ -51,7 +52,7 @@ git clone https://git-wip-us.apache.org/repos/asf/thrift.git
 cd thrift
 ./bootstrap.sh
 ./configure --with-lua=no
-make
+make -j
 make install
 cd ..
 
