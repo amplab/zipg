3d086897ec14a5d3a6aab0d3b202378e0dcc6d47
Anurag Khandelwal <anuragk@berkeley.edu>
2017-02-17 13:35:25 -0800

    Remove logging

--- core/src/SuccinctGraph.cpp
+++ core/src/SuccinctGraph.cpp
@@ -1652,10 +1652,8 @@ void SuccinctGraph::init_rpq_ctx(SuccinctGraph::edge_label label,
     std::vector<int64_t> decoded_dst_ids =
         SuccinctGraphSerde::decode_multi_node_ids(str, dst_id_width);
 
-    for (int64_t dst : decoded_dst_ids) {
-      COND_LOG_E("DSTID: %lld\n", dst);
+    for (int64_t dst : decoded_dst_ids)
       ctx.end_points.insert(SuccinctGraph::path_endpoints(off, dst));
-    }
   }
 }
 
